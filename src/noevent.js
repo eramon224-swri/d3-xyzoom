fc95c351023cdeaa297a7e904a86dbbd72e66d6d
User eramon <eramon@c73b1.electro.swri.edu>
2019-09-20 18:10:26 +0000

    Added allowevent function.

--- src/noevent.js
+++ src/noevent.js
@@ -4,6 +4,10 @@ export function nopropagation() {
   event.stopImmediatePropagation();
 }
 
+export function allowevent() {
+  event.preventDefault();
+}
+
 export default function() {
   event.preventDefault();
   event.stopImmediatePropagation();
